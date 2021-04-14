7b248b0ded3cadc5317d6ef46fe9fd6052171f18
Christoph86 <ChrstphBch@web.de>
2021-04-14 11:14:06 +0200

    AufrufAmZug zu Modal gemacht

--- src/components/SpielZuEnde.js
+++ src/components/SpielZuEnde.js
@@ -10,7 +10,7 @@ const SpielZuEnde = props => {
 
   return (
 
-<div>
+<div className="zu-ende">
       <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} centered>
 
         <Modal.Header>
@@ -33,44 +33,7 @@ const SpielZuEnde = props => {
         </Modal.Footer>
 
       </Modal>
-
     </div>
-
-
-    
-      //   <div class="zu-ende">
-      //     <Button primary data-toggle="modal" data-target="#zu-ende">Du hast die Runde geschafft ✌️</Button>
-      //     {/* Modal */}
-      //     <Modal id="zu-ende" fade>
-      //       <Modal.Dialog centered>
-      //         <Modal.Content>
-      //           <Modal.Header>
-      //             <Modal.Title>Herzlichen Glückwunsch!!</Modal.Title>
-      //           </Modal.Header>
-      //           <Modal.Body>
-      //             Do hast die Runde beendet, möchtest du auf diesem Spielfeld weiter üben?
-      //           </Modal.Body>
-      //           <Modal.Footer centered>
-      //           <Modal.Close>
-      //             <Button
-      //             primary
-      //             onClick={() => {
-      //               props.setSpielfigurPosition(props.spielfigurPosition - props.spielfeldgroesse);
-      //               props.setPopup('aufruf');
-      //             }}
-      //             >
-      //               Weiterspielen
-      //             </Button>
-      //             <Button secondary onClick={() => {props.setPage('startseite'); 
-      //             // props.setSpielfigurPosition(0);
-      //             }} >Zurück zur Startseite</Button>
-      //             </Modal.Close>
-      //           </Modal.Footer>
-      //         </Modal.Content>
-      //       </Modal.Dialog>
-      //     </Modal>
-      //   </div>
-      // );
   )}
 
 export default SpielZuEnde;
