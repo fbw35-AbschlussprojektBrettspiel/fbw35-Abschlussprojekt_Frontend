add0b5912af49ef3dc4b8f9d0f16c48b7ec0591e
TunHuang <huang.tun.anton@gmail.com>
2021-04-28 15:48:14 +0200

    FontAwesomeIcon statt X als Spielfigur mit size xs. Spielfigur.css erstellt für mehr Übersicht

--- src/components/Spielfigur.js
+++ src/components/Spielfigur.js
@@ -1,12 +1,15 @@
 import './Spielfeld.css';
+import './Spielfigur.css';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Spielfigur = props => {
   const spielfigurPositionen = useSelector(state => state.spielfigurPositionen);
 
   return (
     <div className={`spielfeld-${spielfigurPositionen[props.order]} spielfigur spielfigur-${props.order}`}>
-      X
+      <FontAwesomeIcon icon={faUser} size="xs" />
     </div>
   );
 };
