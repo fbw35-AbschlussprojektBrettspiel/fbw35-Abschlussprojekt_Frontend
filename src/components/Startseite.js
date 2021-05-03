fc3655c544018d89354ebacaa453a0965aadbb51
Christoph86 <ChrstphBch@web.de>
2021-05-03 05:30:43 +0200

    Füll-Hintergrund ausserhalb 16:10 auf Startseite hinzugefügt

--- src/components/Startseite.js
+++ src/components/Startseite.js
@@ -41,6 +41,7 @@ function setCSSRatioVars() {
 }
 
   return (
+  <div className="ausserhalbStartseite">
     <section className="Startseite">
       {setCSSRatioVars()}
       <h1>Willkommen auf die Startseite!</h1>
@@ -79,6 +80,7 @@ function setCSSRatioVars() {
         disabled>
       </textarea>
     </section>
+  </div>
   );
 };
 
