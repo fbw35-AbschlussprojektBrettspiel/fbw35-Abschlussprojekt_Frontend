d401956a3f89031bf3e2038833df37cef2686ed3
CoraHell <corinna-hellmund@t-online.de>
2021-05-03 15:18:33 +0200

    überarbeit2

--- src/components/Spielanleitung.js
+++ src/components/Spielanleitung.js
@@ -32,32 +32,33 @@ wieder aufzufrischen.</p>
 
 <ol>
 <li>Spiel starten</li>
-<li>bestimmen der Spielvariante</li>
 <li>allgemeine Regeln</li>
 <li>Felder des Spielfelds</li>
-<li>registrieren und der beste werden</li>
+<li>Registrieren und der Beste werden</li>
 <li>Spielen als Gast</li>
 </ol>
 
 <b>1.</b><br></br>
 <p>
-Eröffne dein eigenes Spielfeld, wählt dazu aus zu welchen
-Technologien du Fragen erhalten möchtet und wie groß das Spielfeld sein soll.
+Eröffne dein eigenes Spielfeld. ("Neues Spiel erstellen")
+Du erhälst eine Spiel-ID.
+Du kannst deinen Namen in das Feld ("Spielername") einfügen.
 </p>
 <b>2.</b><br></br>
 <p>
  Es sind mehrere Spielvarianten möglich, du kannst als Einzelspieler
 für dich selbst üben, oder mit deinen Freunden um den Sieg kämpfen.
+Sende den Homepagelink und Spiel-ID an deine Freunde.
 Warte in dem Fall bis sich alle Mitspieler angemeldet haben und klicke
 dann "Spiel starten".
 </p>
 <b>3.</b><br></br>
 <p>
 Du würfelst, wie weit deine Spielfigur voranschreitet.
-Für eine Quiz-Frage bekommst du 15 Sekunden Zeit.
-Ist die Antwort richtig, kann der nächste Wurf des Würfels starten. 
+Ist die Antwort gegeben, kann der nächste Wurf des Würfels starten. 
 Entweder deiner, oder der des nächsten Mitspielers.
-Bei einer falschen Antwort reist deine Spielfigur jedoch wieder
+Ist die Antwort richtig, bleibt deine Spielfigur stehen.
+Bei einer falschen Antwort reist deine Spielfigur wieder
 zurück, auf das Feld von dem du gekommen bist.
 </p>
 <b>4.</b><br></br>
@@ -67,20 +68,19 @@ Es gibt verschiedene Arten von Spielfeldern,
 dazu zählen vorallem die Quiz-Felder die je nach Bild
 dich bereits vorher sehen lassen zu welchem Themengebiet
 deine Frage gehören wird.
-Weiter gibt es jedoch noch die Aktionsfelder, vielleicht musst du zwei Fragen
-beantworten, hast zwei zur Auswahl, darfst weiter vor oder musst zurück
+Weiter gibt es jedoch noch die Aktionsfelder, diese endscheiden ob
+du weiter vor darfst oder zurück musst
 und noch mehr. Lass dich überraschen.
 </p>
 <b>5.</b><br></br>
 <p>
-Wenn du nicht als Gast spielst wird dein Highscore sowie das Verhälntis an
-richtig beantworteten Fragen gespeichert, und du kannst der Beste werden!
+jeder der Lernen und etwas Spass haben möchte kann natürlich mitmachen,
+in dem Fall wird für dich ein Benutzername (oder "_Spieler") angefügt, um
+keine Verwechslungen zu schaffen. Spaß macht es auch so.
 </p>
 <b>6.</b><br></br>
 <p>
-jeder der lernen und etwas Spass haben möchte kann natürlich mitmachen,
-in dem Fall wird für dich ein Benutzername (oder "guest_") angefügt, um
-keine Verwechslungen zu schaffen. Spaß macht es auch so.
+Gewonnen hat derjenige, der als Erster über das Ziel geht.
 </p>
           </Modal.Body>
   
