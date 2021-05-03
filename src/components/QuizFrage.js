309f4f70e401012b8f37c4cf11f680a561cefe56
Christoph86 <ChrstphBch@web.de>
2021-05-03 02:43:56 +0200

    Fragezeichen Icon bei Frage-Modal eingefügt,... ->space-between fehlt noch

--- src/components/QuizFrage.js
+++ src/components/QuizFrage.js
@@ -4,6 +4,8 @@ import { Modal } from 'react-bootstrap';
 import { useState } from 'react';
 import QuizAntwort from './QuizAntwort';
 import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 const QuizFrage = () => {
 
@@ -15,9 +17,12 @@ const QuizFrage = () => {
   return (
     <section className="quizfrage">
       <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} centered>
-
+      {/* &nbsp; ist die Abkürzung/Lösung um ein Leerzeichen in HTML-Text einzufügen */}
         <Modal.Header>
-          <Modal.Title><p>{frage.frage}</p></Modal.Title >
+          <Modal.Title><p>{frage.frage} &nbsp;&nbsp;&nbsp;          
+                          <FontAwesomeIcon icon={faQuestionCircle} transform="grow-8"
+                          style={{ color: "darkgrey" }} />
+                        </p></Modal.Title >
         </Modal.Header>
 
         <Modal.Body >
