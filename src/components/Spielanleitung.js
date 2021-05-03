f86123d4d43b11d4abcace2194d3d872dac2798c
CoraHell <corinna-hellmund@t-online.de>
2021-05-03 14:01:11 +0200

    spielregeln

--- src/components/Spielanleitung.js
+++ src/components/Spielanleitung.js
@@ -26,8 +26,8 @@ const Spielanleitung = props => {
           </Modal.Header>
   
           <Modal.Body>
-<p>Vielen dank und Spaß an unserem Quiz-Game,
-es wird euch bestimmt helfen etwas zu lernen oder lange vergessenes
+<p>Vielen Dank und Spaß an unserem Quiz-Game,
+es wird dir bestimmt helfen etwas zu lernen oder lange vergessenes
 wieder aufzufrischen.</p>
 
 <ol>
@@ -36,49 +36,50 @@ wieder aufzufrischen.</p>
 <li>allgemeine Regeln</li>
 <li>Felder des Spielfelds</li>
 <li>registrieren und der beste werden</li>
-<li>spielen als Gast</li>
+<li>Spielen als Gast</li>
 </ol>
 
 <b>1.</b><br></br>
 <p>
-Eröffnet euer eigenes Spielfeld, wählt dazu aus zu welchen
-Technologien Ihr Fragen erhalten möchtet und wie groß das Spielfeld sein soll.
+Eröffne dein eigenes Spielfeld, wählt dazu aus zu welchen
+Technologien Du Fragen erhalten möchtet und wie groß das Spielfeld sein soll.
 </p>
 <b>2.</b><br></br>
 <p>
- Es sind mehrere Spielvarianten möglich, ihr könnt als Einzelspieler
-für euch selbst üben, oder mit euren Freunden um den Sieg kämpfen.
-Wartet in dem Fall bis alle sich zu eurem Spiel angemeldet haben und klickt
-dann Spiel starten.
+ Es sind mehrere Spielvarianten möglich, du kannst als Einzelspieler
+für dich selbst üben, oder mit deinen Freunden um den Sieg kämpfen.
+Wartet in dem Fall bis sich alle  Mitspieler angemeldet haben und klickt
+dann "Spiel starten".
 </p>
 <b>3.</b><br></br>
 <p>
-Ihr würfelt, wie weit eure Spielfigur voranschreitet
-Für eine Quiz-Frage bekommt Ihr 15 Sekunden Zeit,
-umso schneller Ihr diese beantwortet desto mehr Punkte gibt es.
-Bei einer falschen Antwort reist eure Spielfigur jedoch wieder
-zurück auf das Feld von dem Ihr gekommen seid.
+Du würfelst, wie weit deine Spielfigur voranschreitet
+Für eine Quiz-Frage bekommst du 15 Sekunden Zeit,
+Ist die Antwort richtig, starte der nächste Wurf des Würfels. 
+Entweder deiner, oder der des nächsten Mitspielers.
+Bei einer falschen Antwort reist es deine Spielfigur jedoch wieder
+zurück, auf das Feld von dem du gekommen seid.
 </p>
 <b>4.</b><br></br>
 <p>
 Die unterschiedlichen Spielfelder:
 Es gibt verschiedene Arten von Spielfeldern,
 dazu zählen vorallem die Quiz-Felder die je nach Bild
-euch bereits vorher sehen lassen zu welchem Themengebiet
-eure Frage gehören wird.
-Weiter gibt es jedoch noch die Aktionsfelder, vielleicht müsst Ihr zwei Fragen
-beantworten, habt zwei zur Auswahl, dürft weiter vor oder müsst zurück
-und noch mehr. Lasst euch überraschen.
+dich bereits vorher sehen lassen zu welchem Themengebiet
+deine Frage gehören wird.
+Weiter gibt es jedoch noch die Aktionsfelder, vielleicht musst du zwei Fragen
+beantworten, hast zwei zur Auswahl, darfst weiter vor oder musst zurück
+und noch mehr. Lass dich überraschen.
 </p>
 <b>5.</b><br></br>
 <p>
-Wenn ihr nicht als Gast spielt wird euer Highscore sowie das Verhälntis an
-richtig beantworteten Fragen gespeichert, und ihr könnt der beste werden!
+Wenn du nicht als Gast spielst wird dein Highscore sowie das Verhälntis an
+richtig beantworteten Fragen gespeichert, und du kannst der Beste werden!
 </p>
 <b>6.</b><br></br>
 <p>
 jeder der lernen und etwas Spass haben möchte kann natürlich mitmachen,
-in dem Fall wird für euren Benutzernamen jedoch "guest_" angefügt, um
+in dem Fall wird für dich ein Benutzername jedoch "guest_" angefügt, um
 keine Verwechslungen zu schaffen. Spaß macht es aber sogar so.
 </p>
           </Modal.Body>
