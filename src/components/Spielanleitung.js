b3793a9a968a2e33cca86d4cec4e2b473fe707b1
TunHuang <huang.tun.anton@gmail.com>
2021-05-04 15:51:41 +0200

    Den Text von Spielanleitung überarbeitet. Außerdem die Fehlermeldungen in Browser Concole beseitigt

--- src/components/Spielanleitung.js
+++ src/components/Spielanleitung.js
@@ -6,93 +6,56 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 const Spielanleitung = props => {
-  
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-  
-    return (
-      <div div class="spielanleitung">
-        <Button variant="primary" className="StartseiteButtons" onClick={handleShow}>
-         Spielanleitung
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <section className="spielanleitung">
+      <Button variant="primary" className="StartseiteButtons" onClick={handleShow}>
+        Spielanleitung
         </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-  
-          <Modal.Header>
-            <Modal.Title>Einleitung und Spielregeln: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faBook} transform="grow-8" style={{ color: "darkgrey" }} />
-            </Modal.Title>
-          </Modal.Header>
-  
-          <Modal.Body>
-<div className = "spiel">
-<p>Vielen Dank und Spaß an unserem Quiz-Game,
-es wird dir bestimmt helfen etwas zu lernen oder lange vergessenes   
-wieder aufzufrischen.</p>
 
-<ol className = "spiel1">
-<li>Spiel erstellen</li>  
-<li>Spiel starten</li>
-<li>allgemeine Regeln</li>
-<li>Felder des Spielfelds</li>
-<li>Benutzername</li>
-<li>Sieg</li>
-</ol>
+      <Modal show={show} onHide={handleClose}>
 
-<b>1.</b><br></br>
-<p>
-Eröffne dein eigenes Spielfeld. ("Neues Spiel erstellen")
-Du erhälst eine Spiel-ID.
-Du kannst deinen Namen in das Feld ("Spielername") einfügen.
-</p>
-<b>2.</b><br></br>
-<p>
-Es sind mehrere Spielvarianten möglich, du kannst als Einzelspieler
-für dich selbst üben, oder mit deinen Freunden um den Sieg kämpfen.
-Sende den Homepagelink und Spiel-ID an deine Freunde.
-Warte in dem Fall bis sich alle Mitspieler angemeldet haben und klicke  
-dann "Spiel starten".
-</p>
-<b>3.</b><br></br>
-<p>
-Du würfelst, wie weit deine Spielfigur voranschreitet.
-Ist die Antwort gegeben, kann der nächste Wurf des Würfels starten. 
-Entweder deiner, oder der des nächsten Mitspielers.
-Ist die Antwort richtig, bleibt deine Spielfigur stehen.
-Bei einer falschen Antwort reist deine Spielfigur wieder
-zurück, auf das Feld von dem du gekommen bist.
-</p>
-<b>4.</b><br></br>
-<p>
-Die unterschiedlichen Spielfelder:
-Es gibt verschiedene Arten von Spielfeldern,
-dazu zählen vorallem die Quiz-Felder die je nach Bild
-dich bereits vorher sehen lassen zu welchem Themengebiet
-deine Frage gehören wird.
-Weiter gibt es jedoch noch die Aktionsfelder, diese endscheiden ob
-du weiter vor darfst oder zurück musst
-und noch mehr. Lass dich überraschen.
-</p>
-<b>5.</b><br></br>
-<p>
-jeder der Lernen und etwas Spass haben möchte kann natürlich mitmachen,
-in dem Fall wird für dich ein Benutzername (oder "_Spieler") angefügt, um
-keine Verwechslungen zu schaffen. Spaß macht es auch so.
-</p>
-<b>6.</b><br></br>
-<p>
-Gewonnen hat derjenige, der als Erster über das Ziel geht.
-</p>
-</div>
+        <Modal.Header>
+          <Modal.Title>Anleitung und Spielregeln: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <FontAwesomeIcon icon={faBook} size="2x" color="darkgrey" />
+          </Modal.Title>
+        </Modal.Header>
 
-          </Modal.Body>
-  
-        </Modal>
-  
-      </div>
-    );
-  }
-  
-  export default Spielanleitung;
+        <Modal.Body>
+          <div className="spiel">
+            <p>Vielen Spaß an unserem Quiz-Brettspiel. Es wird dir bestimmt helfen etwas zu lernen oder lang Vergessenes wieder aufzufrischen.</p>
+
+            <ol className="spiel1">
+              <li>Spiel erstellen</li>
+              <li>Spiel starten</li>
+              <li>Allgemeine Regeln</li>
+              <li>Spielfelder</li>
+              <li>Spielende</li>
+            </ol>
+
+            <b>1.</b><br />
+            <p>Eröffne dein eigenes Spiel. ("Neues Spiel erstellen") Du erhälst eine Spiel-ID, die du an deine Mitspieler senden kannst.</p>
+            <b>2.</b><br />
+            <p>Du kannst als Einzelspieler für dich selbst üben oder mit deinen Freunden um den Sieg kämpfen. Gebe die Spiel-ID und optional einen Spielernamen in die Eingabefelder ein und klicke auf "Spiel beitreten". Wenn sich alle Mitspieler dem Spiel beigetreten sind, dann klickt einer auf "Spiel starten".</p>
+            <b>3.</b><br />
+            <p>Wenn du dran bist, würfelst du, wie weit deine Spielfigur voranschreitet. Wenn du auf einem Quizfeld landest, musst du versuchen, aus mehreren Antworten auf die Richtige zu klicken. Ist die Antwort richtig, bleibt deine Spielfigur stehen. Bei einer falschen Antwort reist deine Spielfigur wieder zurück.</p>
+            <b>4.</b><br />
+            <p>Die Spielfelder: Es gibt verschiedene Arten von Spielfeldern, dazu zählen vorallem die Quiz-Felder, die je nach Icon dich bereits vorher sehen lassen aus welchem Themengebiet deine Frage stammen wird. Weiter gibt es noch die Aktionsfelder, diese lassen dich vor- oder zurückgehen. Lass dich überraschen.</p>
+            <b>5.</b><br />
+            <p>Gewonnen hat derjenige, der als Erster über das Ziel geht. Klicke auf "Spiel beenden", um das Spiel vorzeitig zu beenden.</p>
+          </div>
+
+        </Modal.Body>
+
+      </Modal>
+
+    </section>
+  );
+}
+
+export default Spielanleitung;
