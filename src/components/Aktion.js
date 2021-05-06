ec3c78e35d6078de742093b997e556dcc36dd44a
Christoph86 <ChrstphBch@web.de>
2021-05-06 12:52:20 +0200

    Themenbasierte Quisfragen Icons, text zentriert

--- src/components/Aktion.js
+++ src/components/Aktion.js
@@ -1,3 +1,4 @@
+import './Aktion.css';
 import { Modal, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
@@ -5,6 +6,8 @@ import {
   verschiebeSpielfigur,
   naechsterZug
 } from '../thunks/thunks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Aktion = () => {
 
@@ -26,13 +29,17 @@ const Aktion = () => {
       <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} centered>
         
         <Modal.Header>
-          <Modal.Title>
+          <Modal.Title className="ModalTitle">
+          <FontAwesomeIcon icon={faQuestionCircle} transform="grow-8" style={{ color: "green" }} />
             <p>{aktion.beschreibung}</p>
-            <p>Gehe {aktion.positionsAenderung > 0 ? aktion.positionsAenderung : String(aktion.positionsAenderung).slice(1)} {Math.abs(aktion.positionsAenderung) > 1 ? 'Felder' : 'Feld'} {aktion.positionsAenderung > 0 ? 'vor.' : 'zurück.'}</p>
           </Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>
+        <Modal.Body className="ModalBody">
+        <p>Gehe {aktion.positionsAenderung > 0 ? aktion.positionsAenderung : String(aktion.positionsAenderung).slice(1)} {Math.abs(aktion.positionsAenderung) > 1 ? 'Felder' : 'Feld'} {aktion.positionsAenderung > 0 ? 'vor.' : 'zurück.'}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
           <Button
             variant="primary"
             onClick={() => {
@@ -44,7 +51,7 @@ const Aktion = () => {
           >
             OK
           </Button>
-        </Modal.Body>
+        </Modal.Footer>
 
       </Modal>
     </section>
