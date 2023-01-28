c42a4450e39afd4da16b592f5bf33f79d29fab28
Tun Huang <huang.tun.anton@gmail.com>
2023-01-28 22:48:40 +0100

    Ändere redux-devtools-extension zu dependency und import in store.js zu /developmentOnly, so dass es in Production nicht benutzt wird

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,12 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension'; // die Zeile kann weg für Deployment
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import spielReducer from '../reducers/spielReducer';
 
 const middleware = [thunk];
-
-// Zum Deployment
-// const composedEnhancer = applyMiddleware(...middleware);
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(...middleware));
 
